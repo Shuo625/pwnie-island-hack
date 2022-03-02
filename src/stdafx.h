1465dd876ec9ee7adc6fd4a336ea40ed91855ab7
Shuo626 <qushuo233@gmail.com>
2022-03-02 12:31:45 +0000

    add a file containing mostly used standard header files

---
+++ src/stdafx.h
@@ -0,0 +1,21 @@
+#ifndef STDAFX_H
+#define STDAFX_H
+
+
+#include <dlfcn.h>
+#include <set>
+#include <map>
+#include <functional>
+#include <string>
+#include <vector>
+#include <thread>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+#include <sstream>
+#include <cstdio>
+#include <map>
+#include <iostream>
+
+
+#endif /* STDAFX_H */
