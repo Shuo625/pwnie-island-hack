ae4781fc2a67105f1b5e26a704647409742b1543
Shuo626 <qushuo233@gmail.com>
2022-03-02 12:33:04 +0000

    add game protocol in cpp

--- src/gameProtocol.cpp
+++ src/gameProtocol.cpp
@@ -1,1 +1,23 @@
-#include <
+#include "stdafx.h"
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+#include "commands.h"
+
+
+class Server {
+  public:
+    std::string host;
+    int port;
+
+  public:
+    Server(const std::string& host, int port);
+    void listen();
+};
+
+
+class GameProtocol: public std::thread {
+  public:
+    Server *server;
+        
+};
--- src/gameProtocol.h
+++ src/gameProtocol.h
@@ -2,7 +2,12 @@
 #define GAME_PROTOCOL_H
 
 
+#include <thread>
 
+
+class GameProtocol: public std::thread {
+
+}
 
 
 #endif /* GAME_PROTOCOL_H */
