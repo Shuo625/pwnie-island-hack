6782a3f0abf634aa811af95941056311926fbde2
Shuo626 <qushuo233@gmail.com>
2022-03-07 11:37:37 +0000

    fix bug of concat string

--- src/commands.cpp
+++ src/commands.cpp
@@ -81,97 +81,115 @@ void commandShowEggs() {
     std::string message = "";
     message += "/showEggs";
     message += " ";
-    message += 'First Egg' + " ";
-    message += std::to_string(11604.00) + " ";
-    message += std::to_string(-13131.00) + " ";
+    message += "First_Egg ";
+    message += std::to_string(11604.00);
+    message += " ";
+    message += std::to_string(-13131.00);
+    message += " ";
     message += std::to_string(411.00);
     
     GameProtocol::call_remote_command(message, "127.0.0.1", 8081);
 
     // Construct a message string sent to python server
-    std::string message = "";
+    message = "";
     message += "/showEggs";
     message += " ";
-    message += 'Second Egg' + " ";
-    message += std::to_string(-72667.00) + " ";
-    message += std::to_string(-53567.00) + " ";
+    message += "Second_Egg ";
+    message += std::to_string(-72667.00);
+    message += " ";
+    message += std::to_string(-53567.00);
+    message += " ";
     message += std::to_string(1645.00);
     
     GameProtocol::call_remote_command(message, "127.0.0.1", 8081);
 
     // Construct a message string sent to python server
-    std::string message = "";
+    message = "";
     message += "/showEggs";
     message += " ";
-    message += 'Third Egg' + " ";
-    message += std::to_string(-25045.00) + " ";
-    message += std::to_string(-18085.00) + " ";
+    message += "Third_Egg ";
+    message += std::to_string(-25045.00);
+    message += " ";
+    message += std::to_string(-18085.00);
+    message += " ";
     message += std::to_string(260.00);
     
     GameProtocol::call_remote_command(message, "127.0.0.1", 8081);
 
     // Construct a message string sent to python server
-    std::string message = "";
+    message = "";
     message += "/showEggs";
     message += " ";
-    message += 'Fourth Egg' + " ";
-    message += std::to_string(60453.00) + " ";
-    message += std::to_string(-17409.00) + " ";
+    message += "Fourth_Egg ";
+    message += std::to_string(60453.00);
+    message += " ";
+    message += std::to_string(-17409.00);
+    message += " ";
     message += std::to_string(2939.00);
     
     GameProtocol::call_remote_command(message, "127.0.0.1", 8081);
 
     // Construct a message string sent to python server
-    std::string message = "";
+    message = "";
     message += "/showEggs";
     message += " ";
-    message += 'Fifth Egg' + " ";
-    message += std::to_string(48404.00) + " ";
-    message += std::to_string(28117.00) + " ";
+    message += "Fifth_Egg ";
+    message += std::to_string(48404.00);
+    message += " ";
+    message += std::to_string(28117.00);
+    message += " ";
     message += std::to_string(704.00);
     
     GameProtocol::call_remote_command(message, "127.0.0.1", 8081);
 
     // Construct a message string sent to python server
-    std::string message = "";
+    message = "";
     message += "/showEggs";
     message += " ";
-    message += 'Sixth Egg' + " ";
-    message += std::to_string(65225.00) + " ";
-    message += std::to_string(-5740.00) + " ";
+    message += "Sixth_Egg ";
+    message += std::to_string(65225.00);
+    message += " ";
+    message += std::to_string(-5740.00);
+    message += " ";
     message += std::to_string(4928.00);
     
     GameProtocol::call_remote_command(message, "127.0.0.1", 8081);
 
     // Construct a message string sent to python server
-    std::string message = "";
+    message = "";
     message += "/showEggs";
     message += " ";
-    message += 'Seventh Egg' + " ";
-    message += std::to_string(-51570.00) + " ";
-    message += std::to_string(-61215.00) + " ";
+    message += "Seventh_Egg ";
+    message += std::to_string(-51570.00);
+    message += " ";
+    message += std::to_string(-61215.00);
+    message += " ";
     message += std::to_string(5020.00);
     
     GameProtocol::call_remote_command(message, "127.0.0.1", 8081);
 
     // Construct a message string sent to python server
-    std::string message = "";
+    message = "";
     message += "/showEggs";
     message += " ";
-    message += 'Eighth Egg' + " ";
-    message += std::to_string(24512.00) + " ";
-    message += std::to_string(69682.00) + " ";
+    message += "Eighth_Egg ";
+    message += std::to_string(24512.00);
+    message += " ";
+    message += std::to_string(69682.00);
+    message += " ";
     message += std::to_string(2659.00);
     
     GameProtocol::call_remote_command(message, "127.0.0.1", 8081);
 
     // Construct a message string sent to python server
-    std::string message = "";
+    message = "";
     message += "/showEggs";
     message += " ";
-    message += 'Last Egg' + " ";
-    message += std::to_string(-2778.00) + " ";
-    message += std::to_string(-11035.00) + " ";
+    message += "Last_Egg ";
+    message += std::to_string(-2778.00);
+    message += " ";
+    message += std::to_string(-11035.00);
+    message += " ";
     message += std::to_string(10504.00);
     
     GameProtocol::call_remote_command(message, "127.0.0.1", 8081);
